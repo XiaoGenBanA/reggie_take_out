38d11fae4543e805bc769907e0c74f5efc27d0f6
admin <1046879680@qq.com>
2023-08-03 22:21:57 +0800

    推送

---
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -0,0 +1,14 @@
+package com.itheima.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class,args);
+    }
+}
---
+++ src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.itheima.reggie.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    /**
+     * 指定静态资源不被拦截
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
+}
